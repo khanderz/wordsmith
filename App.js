071be84e370b18ce7854cbc1c59cf6d0574187f3
Khanh <82117702+khanderz@users.noreply.github.com>
2023-08-22 16:02:24 -0700

    bottom nav bar

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {Title} from 'react-native-paper';
+import { BottomNavigationBar } from './src/components/BottomNavigationBar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <Title>My Vocabulary List</Title>
+          <StatusBar style="auto" />
+        </View>
+        <BottomNavigationBar/>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
--- src/components/BottomNavigationBar.tsx
+++ src/components/BottomNavigationBar.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { HomeScreen } from '../views/Home';
 import { AccountScreen } from '../views/Account';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 export const BottomNavigationBar = () => {
@@ -9,8 +10,22 @@ export const BottomNavigationBar = () => {
     const Tab = createMaterialBottomTabNavigator();
 
     return (
-    <Tab.Navigator>  
-        <Tab.Screen name="My Vocabulary Lists" component={HomeScreen} />
+    <Tab.Navigator
+        screenOptions={({ route}) => ({
+            tabBarIcon: ({ focused, color, size}) => {
+                let iconName;
+                if (route.name === 'My Vocabulary Lists') {
+                    iconName = focused ? 'radio-button-on-outline' : 'document-text-outline';
+                } else if (route.name === 'Account/Settings') {
+                    iconName = focused ? 'radio-button-on-outline' : 'person-circle-outline';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />
+            },
+            tabBarActiveTintColor: 'tomato',
+            tabBarInactiveTintColor: 'gray',
+        })}
+    >  
+        <Tab.Screen name="My Vocabulary Lists" component={HomeScreen}  />
         <Tab.Screen name="Account/Settings" component={AccountScreen} />
       </Tab.Navigator>
     )
