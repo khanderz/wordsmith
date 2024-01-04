02c5b7cc4c0f2a2aa1fa5bfd315c83773ca71dde
Khanh <82117702+khanderz@users.noreply.github.com>
2024-01-04 13:28:06 -0600

    add word function success

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,17 +1,41 @@
-import { Box, Flex, Input, Text, Button } from 'native-base';
+import { Box, Flex, Input, Text, Button, FlatList, VStack, useToast } from 'native-base';
 import * as React from 'react';
 
 export const HomeScreen = () => {
-const handleAdd =() => {
-    console.log('add')
-}
+  const toast = useToast();
+  const wordList = []
+
+  const [list, setList] = React.useState(wordList);
+  const [inputValue, setInputValue] = React.useState('');
+
+  const addItem = title => {
+    if (title === "") {
+      toast.show({
+        title: "Please Enter Text",
+      });
+      return;
+    }
+
+    setList(prevList => {
+      return [...prevList, title];
+    });
+  };
+
     return (
           <Flex flexDirection="column" justifyContent="center" align='center' w="100%" h="100%" >
-          <Text margin={1}>Home!</Text>
-          <Box flexDirection="row" justifyContent="center" w="90%" >
-          <Input placeholder='word to add' w="50%" m={1} />
-          <Button m={1} onPress={handleAdd} >Add</Button>
-          </Box>
+            <Text margin={1}>Home!</Text>
+              <Box flexDirection="row" justifyContent="center" w="90%" >
+                <Input placeholder='word to add' w="50%" m={1} value={inputValue} onChangeText={v => setInputValue(v)}   />
+                <Button m={1} onPress={() => {
+            addItem(inputValue);
+            setInputValue("");
+          }}  >Add</Button>
+              </Box>
+          <VStack>
+{list.map((item, index) => 
+  <Text key={index} margin={1}>{item}</Text>
+)}
+          </VStack>
           </Flex>
       );
 }
