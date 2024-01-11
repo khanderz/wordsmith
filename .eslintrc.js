e476efafc45c55a48704f464d8ba0b56a5a81b27
Khanh <82117702+khanderz@users.noreply.github.com>
2024-01-11 11:34:15 -0600

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
-  extends: ['universe/native', 'prettier'],
-}
+  extends: [
+    "universe/native",
+    "prettier",
+    "universe/shared/typescript-analysis",
+  ],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.d.ts"],
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  ],
+};
