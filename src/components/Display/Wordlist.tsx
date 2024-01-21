f3447c8e08c8668fffe474e0699ed507bec2cfd0
Khanh <82117702+khanderz@users.noreply.github.com>
2024-01-21 16:12:13 -0600

    add delete button skeleton, add functionality later

--- src/components/Display/Wordlist.tsx
+++ src/components/Display/Wordlist.tsx
@@ -1,4 +1,5 @@
-import { HStack, Text } from 'native-base'
+import { Button, HStack, Text } from 'native-base'
+import { useClient, useDelete } from 'react-supabase'
 
 import { SeeDefinitionButton } from '../Inputs/SeeDefinitionButton'
 
@@ -13,6 +14,10 @@ export const WordList = ({
   item,
   handleDefinitionButton,
 }: WordlistProps) => {
+  const client = useClient()
+  // const [{ count, data, error, fetching }, execute] = useDelete('todos')
+
+  // const handleDelete = async (index: number) => {}
   return (
     <HStack key={index} w="100%" justifyContent="start" alignItems="center">
       <Text
@@ -28,6 +33,14 @@ export const WordList = ({
         index={index}
         item={item}
       />
+      {/* <Button
+        key={index}
+        aria-label="delete-button"
+        size="sm"
+        onPress={() => handleDelete(index)}
+      >
+        Delete
+      </Button> */}
     </HStack>
   )
 }
