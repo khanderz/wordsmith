ae3cacbdddb6e76a530310083d5dde6faec2fe57
Khanh <82117702+khanderz@users.noreply.github.com>
2024-05-15 14:15:09 -0700

    remove reporter and puppeteer

--- jest.config.ts
+++ jest.config.ts
@@ -22,17 +22,6 @@ const config: Config = {
 
   setupFiles: ['<rootDir>/.jest/setEnvVars.js'],
   verbose: true,
-  reporters: [
-    'default',
-    [
-      'jest-html-reporters',
-      {
-        publicPath: './html-report',
-        filename: 'report.html',
-        expand: true,
-      },
-    ],
-  ],
 }
 
 export default config
