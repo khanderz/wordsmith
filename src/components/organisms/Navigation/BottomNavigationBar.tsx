55165e70ac4c04498fe8c588b3fb4a1f995b3cfe
Khanh <82117702+khanderz@users.noreply.github.com>
2024-09-17 14:28:04 -0700

    update bottom nav

--- src/components/organisms/Navigation/BottomNavigationBar.tsx
+++ src/components/organisms/Navigation/BottomNavigationBar.tsx
@@ -1,35 +1,31 @@
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
-import * as React from 'react'
+import { useState } from 'react'
+import { BottomNavigation, Text } from 'react-native-paper'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
 import { AccountScreen } from '../../../views/Account'
 import { HomeScreen } from '../../../views/Home'
 
-export const BottomNavigationBar = (children: any) => {
-  const Tab = createMaterialBottomTabNavigator()
+export const BottomNavigationBar = () => {
+  const [index, setIndex] = useState(0)
+
+  const [routes] = useState([
+    { key: 'home', title: 'Home', icon: 'home' },
+    { key: 'account', title: 'Account', icon: 'person-circle-outline' },
+  ])
+
+  const renderScene = BottomNavigation.SceneMap({
+    home: HomeScreen,
+    account: AccountScreen,
+  })
 
   return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color }) => {
-          let iconName
-          if (route.name === 'My Vocabulary Lists') {
-            iconName = focused
-              ? 'radio-button-on-outline'
-              : 'document-text-outline'
-          } else if (route.name === 'Account/Settings') {
-            iconName = focused
-              ? 'radio-button-on-outline'
-              : 'person-circle-outline'
-          }
-          return <Ionicons name={iconName} color={color} />
-        },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
-      })}
-    >
-      <Tab.Screen name="My Vocabulary Lists" component={HomeScreen} />
-      <Tab.Screen name="Account/Settings" component={AccountScreen} />
-    </Tab.Navigator>
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+      renderIcon={({ route, color }) => (
+        <Ionicons name={route.icon} size={26} color={color} />
+      )}
+    />
   )
 }
