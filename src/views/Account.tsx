c3b15cd250f1b409a455957db71a7f56f9f3a320
Khanh <82117702+khanderz@users.noreply.github.com>
2024-09-18 15:52:37 -0700

    update account view

--- src/views/Account.tsx
+++ src/views/Account.tsx
@@ -1,13 +1,23 @@
-import { View } from 'react-native'
+import { View, ActivityIndicator } from 'react-native'
 
 import { UserProfile } from '../components/organisms/Display/UserProfile'
 import { Login } from '../components/organisms/Inputs/Login'
+import { useUser } from '../lib/providers/UserProvider'
 
 export const AccountScreen = () => {
+  const { user, loading } = useUser()
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Login />
-      <UserProfile />
+      {user ? <UserProfile /> : <Login />}
     </View>
   )
 }
